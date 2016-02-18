9d5860231995622d94c6272876f2089ca692ef9c
Tomas Leffew <tleffew1994@gmail.com>
2016-02-18 15:44:14 -0600

    adding all my work

--- public/js/tictactoe.js
+++ public/js/tictactoe.js
@@ -23,6 +23,7 @@ $(".arrayRow5").on("click",arrayRow5Push);
 $(".arrayRow6").on("click",arrayRow6Push);
 $(".arrayRow7").on("click",arrayRow7Push);
 $(".arrayRow8").on("click",arrayRow8Push);
+$(".arrayRowAll").on("click",winTicTacToe)
 
 
 function buttonEvent(event){
@@ -42,49 +43,44 @@ function buttonEvent(event){
 function arrayRow1Push(event){
     arrayRow1.push(this.innerHTML);
     arrayAll.push(this.innerHTML);
-    winTicTacToe(event);
+    console.log(arrayAll);
     clearAllForTie(event);
 }
 
 function arrayRow2Push(event){
     arrayRow2.push(this.innerHTML);
     arrayAll.push(this.innerHTML);
-    winTicTacToe(event);
+    console.log(arrayAll);
     clearAllForTie(event);
 }
 
 function arrayRow3Push(event){
     arrayRow3.push(this.innerHTML);
     arrayAll.push(this.innerHTML);
-    winTicTacToe(event);
     clearAllForTie(event);
 }
 
 function arrayRow4Push(event){
     arrayRow4.push(this.innerHTML);
     arrayAll.push(this.innerHTML);
-    winTicTacToe(event);
     clearAllForTie(event);
 }
 
 function arrayRow5Push(event){
     arrayRow5.push(this.innerHTML);
     arrayAll.push(this.innerHTML);
-    winTicTacToe(event);
     clearAllForTie(event);
 }
 
 function arrayRow6Push(event){
     arrayRow6.push(this.innerHTML);
     arrayAll.push(this.innerHTML);
-    winTicTacToe(event);
     clearAllForTie(event);
 }
 
 function arrayRow7Push(event){
     arrayRow7.push(this.innerHTML);
     arrayAll.push(this.innerHTML);
-    winTicTacToe(event);
     clearAllForTie(event);
 
 }
@@ -92,17 +88,16 @@ function arrayRow7Push(event){
 function arrayRow8Push(event){
     arrayRow8.push(this.innerHTML);
     arrayAll.push(this.innerHTML);
-    winTicTacToe(event);
     clearAllForTie(event);
 }
 
 function winTicTacToe(event){
     if (arrayRow1.join() == threeX.join() || arrayRow2.join() == threeX.join() || arrayRow3.join() == threeX.join() || arrayRow4.join() == threeX.join() || arrayRow5.join() == threeX.join() || arrayRow6.join() == threeX.join() || arrayRow7.join() == threeX.join() || arrayRow8.join() == threeX.join()) {
-        $(".btn").html("&nbsp;");
         $(".btn").removeAttr('disabled');
+        $(".btn").html("&nbsp;");
         countForX++;
         $(".player1Count").html("Player X score: " + countForX)
-        buttonOperator = "x";
+        buttonOperator = "o";
         arrayRow1 = [];
         arrayRow2 = [];
         arrayRow3 = [];
@@ -115,9 +110,9 @@ function winTicTacToe(event){
     }
 
     if (arrayRow1.join() == threeO.join() || arrayRow2.join() == threeO.join() || arrayRow3.join() == threeO.join() || arrayRow4.join() == threeO.join() || arrayRow5.join() == threeO.join() || arrayRow6.join() == threeO.join() || arrayRow7.join() == threeO.join() || arrayRow8.join() == threeO.join()) {
-        $(".btn").html("&nbsp;");
         $(".btn").removeAttr('disabled');
-        buttonOperator = "o";
+        $(".btn").html("&nbsp;");
+        buttonOperator = "x";
         countForO++;
         $(".player2Count").html("Player O score: " + countForO)
         arrayRow1 = [];
@@ -133,7 +128,7 @@ function winTicTacToe(event){
 }
 
 function clearAllForTie(event){
-    var arrayTieArray = ["x", "x", "x", "o", "o", "x", "x", "x", "o", "o", "o", "o", "x", "x", "o", "o", "x", "x", "o", "o", "o", "x", "x", "x"]
+    var arrayTieArray = ["x", "x", "x", "o", "o", "x", "x", "x", "o", "o", "o", "o", "x", "x", "o", "o", "x", "x", "o", "o", "o", "x", "x", "x"];
     if (arrayAll.join().length == arrayTieArray.join().length) {
         $(".btn").html("&nbsp;");
         $(".btn").removeAttr('disabled');
